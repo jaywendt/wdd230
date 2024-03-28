0f3494ac9acf1875d8ad9aa07c08fe6351a6d114
jwendt <jaywendt1993@gmail.com>
2024-03-28 18:53:06 -0500

    weather

---
+++ week05/scripts/weather.js
@@ -0,0 +1,26 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=5c267d8cd95cc207aa4443050642a060'
+
+async function apiFetch(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.displayResults}&deg;F`;
+    const iconsrc = `https://openeathermap.org/img/w/${}`
+}
+
+apiFetch();
