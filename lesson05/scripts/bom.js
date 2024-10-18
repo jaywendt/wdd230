45ea270e3969ba29e3749b8e2d2d0388bdfefae3
jaywendt93 <jaywendt1993@gmail.com>
2024-10-17 20:24:40 -0500

    bom

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -2,4 +2,18 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('list');
 
-button.addEventListener('click', function() { ... });
+button.addEventListener('click', function() {
+    if (input.value != '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(li);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        input.focus();
+        input.value = '';
+    }
+});
