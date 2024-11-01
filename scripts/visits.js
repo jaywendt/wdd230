ba7b2cee6570cad303baac7ad91f2168d38f521f
jaywendt <jaywendt1993@gmail.com>
2024-10-31 22:29:16 -0500

    visits

---
+++ scripts/visits.js
@@ -0,0 +1,12 @@
+const visitDisplay = document.querySelector('.visits');
+
+let numVisits = Number(window.localStorage.getItem(numVisits-ls)) || 0;
+if (numVisits !== 0) {
+    visitDisplay.textContent = numVisits;
+} else {
+    visitDisplay.textContent = 'Welcome to my home page!';
+}
+
+numVisits++;
+
+localStorage.setItem('numVisits-ls', numVisits);
