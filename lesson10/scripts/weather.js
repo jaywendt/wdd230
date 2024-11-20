70b5a0a522f1f9768f5b59e3c38c8f8bcb1dce8e
jaywendt <jaywendt1993@gmail.com>
2024-11-19 20:56:08 -0600

    weather

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,29 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=5c267d8cd95cc207aa4443050642a060&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            /*console.log(data);*/
+            displayResults(data);
+        } else {
+            throw error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = '${data.main.temp}&deg;F';
+    const iconsrc = 'https://openweathermap.org/img/w/${data.weather[0].icon}.png'
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', 'weather icon');
+}
+
+apiFetch();
