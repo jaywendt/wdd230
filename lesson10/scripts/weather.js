66f3b0145611c489b04886f6b8b0f72aeaea154a
jaywendt <jaywendt1993@gmail.com>
2024-11-21 19:08:06 -0600

    weather

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,4 +1,4 @@
-const currentTemp = document.querySelector('#current-temp');
+const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
@@ -20,11 +20,11 @@ async function apiFetch(){
 
 function displayResults(data) {
     currentTemp.innerHTML = '${data.main.temp}&deg;F';
-    const iconsrc = 'https://openweathermap.org/img/w/${data.weather[0].icon}.png'
+    const iconsrc = 'https://openweathermap.org/img/wn/10d@2x.png'
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', 'weather icon');
-    captionDesc.textContent = '{desc}';
+    captionDesc.textContent = desc;
 }
 
 apiFetch();
